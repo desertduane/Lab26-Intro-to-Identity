09019da9db40221c941d5f43d20009d155c3a553
duane rush <desertduane@yahoo.com>
2017-10-17 09:51:06 -0700

    added controllers

---
+++ Lab26Idenity/Lab26Idenity/Data/Lab26IdenityContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab26Idenity.Models
+{
+    public class Lab26IdenityContext : DbContext
+    {
+        public Lab26IdenityContext (DbContextOptions<Lab26IdenityContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lab26Idenity.Models.CMS> CMS { get; set; }
+    }
+}
