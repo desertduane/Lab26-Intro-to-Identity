22241e0bdc46cbad5dda7e05fd366ec7daa1597a
duane rush <desertduane@yahoo.com>
2017-10-17 09:40:35 -0700

    add mvc folders nuget package and set up start up.

---
+++ Lab26Idenity/Lab26Idenity/Models/CMS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab26Idenity.Models
+{
+    public class CMS
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public bool IsReleased { get; set; }
+    }
+}
